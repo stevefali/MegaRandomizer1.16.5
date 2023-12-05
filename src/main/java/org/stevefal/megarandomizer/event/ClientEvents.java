aa8a9ae096f9eb41db226ceb73f52e4d9264a906
stevefali <stevefaliszewski@gmail.com>
2023-12-05 02:42:34 -0800

    Add ModPauseScreen and register pause screen event, **Remembered to stage changes**

--- src/main/java/org/stevefal/megarandomizer/event/ClientEvents.java
+++ src/main/java/org/stevefal/megarandomizer/event/ClientEvents.java
@@ -1,2 +1,28 @@
-package org.stevefal.megarandomizer.event;public class ClientEvents {
+package org.stevefal.megarandomizer.event;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.screen.IngameMenuScreen;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.client.event.GuiScreenEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import org.stevefal.megarandomizer.MegaRandomizer1_16_5;
+import org.stevefal.megarandomizer.gui.ModPauseScreen;
+
+@OnlyIn(Dist.CLIENT)
+public class ClientEvents {
+
+    @Mod.EventBusSubscriber(modid = MegaRandomizer1_16_5.MODID, value = Dist.CLIENT)
+    public static class ClientForgeEvents {
+
+        // Intercept the stock IngameMenuScreen and instead display ModPauseScreen
+        @SubscribeEvent
+        public static void onPauseMenuTriggered(GuiScreenEvent.InitGuiEvent event) {
+            boolean isSinglePlayer = Minecraft.getInstance().isLocalServer();
+            if (event.getGui() instanceof IngameMenuScreen) {
+                Minecraft.getInstance().setScreen(new ModPauseScreen(true));
+            }
+        }
+    }
 }
--- src/main/java/org/stevefal/megarandomizer/gui/MegaRandomOptionsScreen.java
+++ src/main/java/org/stevefal/megarandomizer/gui/MegaRandomOptionsScreen.java
@@ -1,2 +1,27 @@
-package org.stevefal.megarandomizer.gui;public class MegaRandomOptionsScreen {
+package org.stevefal.megarandomizer.gui;
+
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TranslationTextComponent;
+import net.minecraft.world.World;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+
+@OnlyIn(Dist.CLIENT)
+public class MegaRandomOptionsScreen extends Screen {
+
+    private final Screen lastScreen;
+
+    private final World world;
+
+    private final boolean showMegaRandomOptions;
+
+
+    protected MegaRandomOptionsScreen(Screen lastScreen, World world, boolean showMegaRandomOptions) {
+        super(new TranslationTextComponent("menu.megarandomoptions"));
+
+        this.lastScreen = lastScreen;
+        this.world = world;
+        this.showMegaRandomOptions = showMegaRandomOptions;
+    }
 }
